b34dfe9276f045f997fcf255767e1beab07edbd0
Pedro Galan <pedro.galan@jobandtalent.com>
2022-02-23 21:22:53 +0100

    Add controller and view to show users

--- rumbl/lib/rumbl_web/router.ex
+++ rumbl/lib/rumbl_web/router.ex
@@ -17,6 +17,8 @@ defmodule RumblWeb.Router do
   scope "/", RumblWeb do
     pipe_through :browser
 
+    get "/users", UserController, :index
+    get "/users/:id", Usercontroller, :show
     get "/", PageController, :index
   end
 
