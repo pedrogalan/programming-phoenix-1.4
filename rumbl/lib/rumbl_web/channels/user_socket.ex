68241a1ea94b44185a3d36dd97f79cf300aebf54
Pedro Galan <pedro.galan@jobandtalent.com>
2022-03-20 16:01:44 +0100

    Channels working

---
+++ rumbl/lib/rumbl_web/channels/user_socket.ex
@@ -0,0 +1,12 @@
+defmodule RumblWeb.UserSocket do
+  use Phoenix.Socket
+
+  ## Channels
+  channel "videos:*", RumblWeb.VideoChannel
+
+  def connect(_params, socket, _connect_info) do
+    {:ok, socket}
+  end
+
+  def id(_socket), do: nil
+end
